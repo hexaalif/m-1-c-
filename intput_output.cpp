ae11cd3475932cf79510ec69b9e63d0511bc3cb3
hexaalif <hexaalif2020@gmail.com>
2023-05-21 13:10:28 +0600

    works

--- intput_output.cpp
+++ intput_output.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 
 int main()
 {
@@ -10,7 +10,9 @@ int main()
     // std::cout << "hello" << std::endl
     //           << "world";
 
-    int a = 100;
-    std::cout << a;
+    int a = 100, b = 200;
+    // std::cout << a;
+    // std::cout << a << b;
+    std::cout << a << " " << b;
     return 0;
 }
