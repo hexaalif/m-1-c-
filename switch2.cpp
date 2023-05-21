591d2af614ae9041a7c94f9a8edc5fc776af3be9
hexaalif <hexaalif2020@gmail.com>
2023-05-21 16:02:59 +0600

    swtich case

---
+++ switch2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    cin >> a;
+    switch (a)
+    {
+    case 'a':
+        /* code */
+        cout << "Vowel" << endl;
+        break;
+    case 'e':
+        /* code */
+        cout << "Vowel" << endl;
+        break;
+    case 'i':
+        /* code */
+        cout << "Vowel" << endl;
+        break;
+
+    case 'o':
+        /* code */
+        cout << "Vowel" << endl;
+        break;
+    case 'u':
+        /* code */
+        cout << "Vowel" << endl;
+        break;
+    default:
+        cout << "Consonent" << endl;
+        break;
+    }
+}
